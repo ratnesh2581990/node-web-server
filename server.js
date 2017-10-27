62d3d8fed49d27e59a3968e65f1e296e1a2d2f71
Praveen Rathore <praveen.rathore@venlogic.com>
2017-10-27 16:25:02 +0530

    projects page add

--- server.js
+++ server.js
@@ -42,6 +42,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'project Page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage : 'Unable to Provide the page you are requesting'

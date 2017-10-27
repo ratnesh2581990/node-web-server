d2f5533adab216e68322cc9e705dd8cb2bb5e6df
Praveen Rathore <praveen.rathore@venlogic.com>
2017-10-27 15:28:00 +0530

    second commit for heroku update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -46,6 +47,6 @@ app.get('/bad', (req, res) => {
     errorMessage : 'Unable to Provide the page you are requesting'
   });
 });
-app.listen(3000, () => {
-  console.log('server is running on port 3000');
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
